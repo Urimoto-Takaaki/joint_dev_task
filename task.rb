7a464919fdaf46abfaa1dec7592f4d4f572996ea
Urimoto-Takaaki <69062224+Urimoto-Takaaki@users.noreply.github.com>
2020-08-06 20:33:38 +0900

    Q17

--- task.rb
+++ task.rb
@@ -160,7 +160,18 @@ end
 
 class UserQ17
   # 以下に回答を記載
-
+  def initialize(name:,age:,gender:)
+    @name = name
+    @age = age
+    @gender = gender
+  end
+  def info
+    puts <<~EOS
+    名前:#{@name}
+    年齢:#{@age}
+    性別:#{@gender}
+    EOS
+  end
 end
 
 def q17
